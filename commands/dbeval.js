33dfadd8ee28bbc278106f972975dd83b05b4dfb
Paul <witlesshydra@outlook.com>
2020-08-27 20:43:27 +0200

    some minor fixes

--- commands/dbeval.js
+++ commands/dbeval.js
@@ -2,8 +2,7 @@ module.exports = {
   name: "dbeval",
   run: async (client, message, args, db) => {
     switch(message.author.id){
-      case '524643550546362378':
-      case '146681835491950592':
+      case '{add_here_your_user_id}':
           await db.eval(args).then((res) => {
             message.channel.send(JSON.stringify(res));
           })
--- database/watcher.js
+++ database/watcher.js
@@ -3,6 +3,18 @@ exports.init = async (discord_client, message_embed) => {
   const schedule = require("node-schedule-tz");
   const conn2 = await require("./connector").init();
   const client = discord_client;
+  /**
+   * In case there wasn't already a hash, create them
+   * */
+  conn2.hgetall("cachedversions", (err, val) => {
+      if(err || !val )
+          conn2.hset("cachedversions", "0", "0")
+  })
+  conn2.hgetall("guilds", (err, val) => {
+      if(err || !val){
+          conn2.hset("guilds", "0", "0")
+      }
+  })
   if(client && conn2){
     schedule.scheduleJob('*/2 * * * * *', () => {  //Execute every minute
       watcher(conn2, client, message_embed);
@@ -19,66 +31,66 @@ watcher = async (conn, client, message_embed) => {
     conn.hgetall("cachedversions", async (err, val) => {
 
         for (let id in val) {
-
-            let cached_v = await dbGetV("cachedversions", id);
+            if (id !== "0"){
+                let cached_v = await dbGetV("cachedversions", id);
             let current_v = await dbGetV(id, "_v");
             let flagIncrement = 1
 
-            if(cached_v !== current_v){ //This means there was something changed from previous version
+            if (cached_v !== current_v) { //This means there was something changed from previous version
 
-              let hash_table = await require("./database").getAll(id); //Get the hash_table from the provided ID
-              let server_id = id.split('_')[0];
-              let channel_id = hash_table.channel;
-              let message_id = hash_table.message;
+                let hash_table = await require("./database").getAll(id); //Get the hash_table from the provided ID
+                let server_id = id.split('_')[0];
+                let channel_id = hash_table.channel;
+                let message_id = hash_table.message;
 
-              let sortable = [];   //Initialize sortable array to sort the object return from hgetall
+                let sortable = [];   //Initialize sortable array to sort the object return from hgetall
 
-                 for (var idnt in hash_table) {
-                  if(idnt !== "_v" && idnt !== "message" && idnt!== "channel") //Exclude env from sorting
-                    sortable.push([idnt, hash_table[idnt]]);
-                 }
+                for (var idnt in hash_table) {
+                    if (idnt !== "_v" && idnt !== "message" && idnt !== "channel") //Exclude env from sorting
+                        sortable.push([idnt, hash_table[idnt]]);
+                }
 
-              sortable.sort(function(a, b) {
-                  return b[1] - a[1];
-              });
-
-
-              //DISCORD STUFF
-
-              let stringToSend = "";
-              for (let i = 0; i < sortable.length; i++){
-                stringToSend += "\n<@" + sortable[i][0] + "> > " + sortable[i][1] + "\n"
-              }
-
-              const embed = new message_embed()
-
-              await client.guilds.cache.get(server_id).channels.cache.get(channel_id).messages.fetch(message_id)
-                .then((msg) => {
-
-                  let embed_title = msg.embeds[0].title;
-                  let embed_color = msg.embeds[0].color;
-
-                  embed
-
-                    .setTitle(embed_title)
-                    .setColor(embed_color)
-                    .setDescription(stringToSend)
-                    .setFooter(id);
-
-                  msg.edit(embed)
-
-                })
-                .catch(() => {
-                  const drop = require("./database").dropTop
-                  drop(id);
-                  flagIncrement = 0;
-                })
+                sortable.sort(function (a, b) {
+                    return b[1] - a[1];
+                });
 
 
+                //DISCORD STUFF
 
-              if(flagIncrement)
-               conn.hset("cachedversions", id, current_v)
+                let stringToSend = "";
+                for (let i = 0; i < sortable.length; i++) {
+                    stringToSend += "\n<@" + sortable[i][0] + "> > " + sortable[i][1] + "\n"
+                }
+
+                const embed = new message_embed()
+
+                await client.guilds.cache.get(server_id).channels.cache.get(channel_id).messages.fetch(message_id)
+                    .then((msg) => {
+
+                        let embed_title = msg.embeds[0].title;
+                        let embed_color = msg.embeds[0].color;
+
+                        embed
+
+                            .setTitle(embed_title)
+                            .setColor(embed_color)
+                            .setDescription(stringToSend)
+                            .setFooter(id);
+
+                        msg.edit(embed)
+
+                    })
+                    .catch(() => {
+                        const drop = require("./database").dropTop
+                        drop(id);
+                        flagIncrement = 0;
+                    })
+
+
+                if (flagIncrement)
+                    conn.hset("cachedversions", id, current_v)
             }
-         }
+        }
+      }
     })
 }
